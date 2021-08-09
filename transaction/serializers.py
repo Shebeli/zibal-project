42f65a4c796d863c0b0fcae1d30dc27c7edcca41
shervin <shervinsaeedi78@gmail.com>
2021-08-09 12:31:50 +0430

    🔥 Remove unnecessary imports from serializers

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -1,12 +1,9 @@
 from datetime import datetime
 
-import jdatetime
 from rest_framework import serializers
 from rest_framework_mongoengine.fields import ObjectIdField
-from rest_framework_mongoengine.serializers import DocumentSerializer
 
 from transaction.documents import Transaction
-from transaction.utils import fa_month_to_str
 
 
 class TRTypeSerializer(serializers.Serializer):
